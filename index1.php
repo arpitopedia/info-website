e90c151955e009e5ab854a9cac27687b840936d0
arpitopedia <84175900+arpitopedia@users.noreply.github.com>
2024-11-23 18:24:58 +0530

    index1.php

--- index1.php
+++ index1.php
@@ -3,24 +3,25 @@ require 'PHPMailerAutoload.php';
 
 $mail = new PHPMailer;
 
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+// Uncomment the line below for verbose debugging (optional)
+// $mail->SMTPDebug = 3; 
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp1.gmail.com';  // Specify main and backup SMTP servers
+$mail->Host = 'smtp.gmail.com';                       // Corrected SMTP server
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'sickso518@gmail.com';                 // SMTP username
-$mail->Password = 'gujarat123';                           // SMTP password
-$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587;                                    // TCP port to connect to
+$mail->Username = 'sickso518@gmail.com';              // SMTP username
+$mail->Password = 'gujarat123';                       // SMTP password or App Password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption
+$mail->Port = 587;                                    // Correct port for TLS
 
 $mail->setFrom('sickso518@gmail.com', 'My info website');
-$mail->addAddress('as4820000@gmail.com');                 // Name is optional
+$mail->addAddress('as4820000@gmail.com');             // Add recipient
 $mail->addReplyTo('sickso518@gmail.com');
 $mail->isHTML(true);                                  // Set email format to HTML
 
 $mail->Subject = 'gmail my info website';
-$mail->Body    = '<h1 align=center>thank you</h1><br><h4 align=center>for contacting us</h4>';
-$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+$mail->Body    = '<h1 align=center>Thank you</h1><br><h4 align=center>for contacting us</h4>';
+$mail->AltBody = 'This is the plain text body for non-HTML clients';
 
 if(!$mail->send()) {
     echo 'Message could not be sent.';
